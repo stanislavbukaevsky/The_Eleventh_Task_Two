67b677df6993457142e10c0d333571b39d2f4c19
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-10-24 14:52:51 +0300

    Первый коммит

---
+++ src/test/java/pro/sky/employeebook/EmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
